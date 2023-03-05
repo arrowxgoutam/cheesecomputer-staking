a5906a8a1b12ccf461a9084ae0946dfa926717a7
“arrowxgoutam” <goutampaik42@gmail.com>
2023-03-06 01:39:47 +0600

    v3

--- common/Footer.tsx
+++ common/Footer.tsx
@@ -1,6 +1,7 @@
 import { darken, lighten } from 'polished'
 import { FaDiscord, FaGithub, FaMedium, FaTwitter } from 'react-icons/fa'
 
+import { contrastColorMode } from './utils'
 
 export const SOCIALS = {
   discord: { icon: <FaDiscord />, link: 'https://discord.gg/byq6uNTugq' },
@@ -18,13 +19,23 @@ export const Footer = ({
 }) => {
   return (
     <div
-      className="px-10 md:px-32"
+      className=""
       style={{
         background: darken(0.03, bgColor),
       }}
     >
+      <div className="f-con">
+        <p className="text-primary text-center mb-5 pt-10">Copyright © 2022, CheeseComputer</p>
+        <p className="text-primary text-center mb-5"><a href="https://twitter.com/CheeseComp_NFT" target="_blank">Twitter</a></p>
+        <p className="text-primary text-center mb-5"><a href="https://www.etsy.com/shop/CheeseComputer" target="_blank">Etsy</a></p>
+        <p className="text-primary text-center mb-5"><a href="https://www.instagram.com/cheesecomputer8/" target="_blank">Instagram</a></p>
+        <p className="text-primary text-center pb-10"><a href="https://cheesecomputer8.medium.com/" target="_blank">Medium Blogs</a></p>
       
-     
+      </div>
+      
+      {/* <div className="text-md flex flex-row justify-center font-medium">
+        Copyright 2022 Cardinal Labs. All rights reserved
+      </div> */}
     </div>
   )
 }
