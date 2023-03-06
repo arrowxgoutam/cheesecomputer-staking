daa08afc538552ad38df43b946ade5b02a08375d
“arrowxgoutam” <goutampaik42@gmail.com>
2023-03-06 15:36:21 +0600

    social footer bugs fix

--- common/Footer.tsx
+++ common/Footer.tsx
@@ -1,7 +1,12 @@
 import { darken, lighten } from 'polished'
 import { FaDiscord, FaGithub, FaMedium, FaTwitter } from 'react-icons/fa'
 
-
+export const SOCIALS = {
+  discord: { icon: <FaDiscord />, link: 'https://discord.gg/byq6uNTugq' },
+  github: { icon: <FaGithub />, link: 'https://github.com/cardinal-labs' },
+  medium: { icon: <FaMedium />, link: 'https://cardinal-labs.medium.com/' },
+  twitter: { icon: <FaTwitter />, link: 'https://twitter.com/cardinal_labs' },
+}
 export const Footer = ({
   bgColor = 'rgb(26, 27, 32)',
 }: {
